43400c241d2330450fdd4a0d7df7ad529d477d82
GeGaoJian <280856888@qq.com>
2017-12-05 15:39:10 +0800

    first commit

---
+++ src/main/java/com/iip/datafusion/DatafusionApplication.java
@@ -0,0 +1,12 @@
+package com.iip.datafusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatafusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatafusionApplication.class, args);
+	}
+}
