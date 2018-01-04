a99bd840cfe942aca2bb5c0baf5e6ce713318d79
viking18 <374165072@qq.com>
2018-01-04 14:28:18 +0800

    create develop

---
+++ src/main/java/com/iip/datafusion/dgs/controller/DgsController.java
@@ -0,0 +1,7 @@
+package com.iip.datafusion.dgs.controller;
+
+/**
+ * Created by jingwei on 2018/1/4.
+ */
+public class dgsController {
+}
---
+++ src/main/java/com/iip/datafusion/dgs/service/DgsService.java
@@ -0,0 +1,7 @@
+package com.iip.datafusion.dgs.service;
+
+/**
+ * Created by jingwei on 2018/1/4.
+ */
+public class DgsService {
+}
