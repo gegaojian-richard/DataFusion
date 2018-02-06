1933b4eb7c7ce22896d61a8f320c26dea2e6f43f
viking18 <374165072@qq.com>
2018-02-06 22:21:16 +0800

    修改JOB接口，修改INT问题

--- src/main/java/com/iip/datafusion/backend/job/Job.java
+++ src/main/java/com/iip/datafusion/backend/job/Job.java
@@ -1,4 +1,10 @@
 package com.iip.datafusion.backend.job;
 
 public interface Job {
+    String JobID="";
+
+    String getUserID();
+    JobType getJobType();
+    String getJobID();
+    String getDescription();
 }
