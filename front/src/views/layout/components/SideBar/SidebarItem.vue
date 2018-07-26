4a5110353e19db1bdfe89f2bd572065f6270f6f3
zengc <njuzengc@foxmail.com>
2018-07-26 17:13:06 +0800

    取消侧边栏

--- front/src/views/layout/components/SideBar/SidebarItem.vue
+++ front/src/views/layout/components/SideBar/SidebarItem.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="menu-wrapper">
-    <template v-for="(item, index) in routes" v-if="!item.hidden&&item.children">
+    <!--<template v-for="(item, index) in routes" v-if="!item.hidden&&item.children">-->
 
-      <router-link v-if="(item.children.length===1 && !item.children[0].children )|| !item.children[1].name" :to="item.path+'/'+item.children[0].path" :key="item.children[0].name">
-        <el-menu-item  style="text-align:left;" :index="item.path+'/'+item.children[0].path" :class="{'submenu-title-noDropdown':!isNest}">
-          <!--<svg-icon v-if="item.children[0].meta&&item.children[0].meta.icon" :icon-class="item.children[0].meta.icon"></svg-icon>-->
-          <img :src="'/static/home/' + (index - 2) + '.png'" alt="">
-          <span v-if="item.children[0].meta&&item.children[0].meta.title">{{item.children[0].meta.title}}</span>
-        </el-menu-item>
-      </router-link>
+      <!--<router-link v-if="(item.children.length===1 && !item.children[0].children )|| !item.children[1].name" :to="item.path+'/'+item.children[0].path" :key="item.children[0].name">-->
+        <!--<el-menu-item  style="text-align:left;" :index="item.path+'/'+item.children[0].path" :class="{'submenu-title-noDropdown':!isNest}">-->
+          <!--&lt;!&ndash;<svg-icon v-if="item.children[0].meta&&item.children[0].meta.icon" :icon-class="item.children[0].meta.icon"></svg-icon>&ndash;&gt;-->
+          <!--<img :src="'/static/home/' + (index - 2) + '.png'" alt="">-->
+          <!--<span v-if="item.children[0].meta&&item.children[0].meta.title">{{item.children[0].meta.title}}</span>-->
+        <!--</el-menu-item>-->
+      <!--</router-link>-->
 
-      <el-submenu style="text-align:left;" v-else :index="item.name||item.path" :key="item.name">
-        <template slot="title">
-          <!--<svg-icon  v-if="item.meta&&item.meta.icon" :icon-class="item.meta.icon"></svg-icon>-->
-          <img src="/static/home/8.png" alt="">
-          <span v-if="item.meta&&item.meta.title">{{item.meta.title}}</span>
-        </template>
+      <!--<el-submenu style="text-align:left;" v-else :index="item.name||item.path" :key="item.name">-->
+        <!--<template slot="title">-->
+          <!--&lt;!&ndash;<svg-icon  v-if="item.meta&&item.meta.icon" :icon-class="item.meta.icon"></svg-icon>&ndash;&gt;-->
+          <!--<img src="/static/home/8.png" alt="">-->
+          <!--<span v-if="item.meta&&item.meta.title">{{item.meta.title}}</span>-->
+        <!--</template>-->
 
-        <template v-for="child in item.children" v-if="!child.hidden">
-          <sidebar-item :is-nest="true" class="nest-menu" v-if="child.children&&child.children.length>0" :routes="[child]" :key="child.path"></sidebar-item>
+        <!--<template v-for="child in item.children" v-if="!child.hidden">-->
+          <!--<sidebar-item :is-nest="true" class="nest-menu" v-if="child.children&&child.children.length>0" :routes="[child]" :key="child.path"></sidebar-item>-->
 
-          <router-link v-else :to="item.path+'/'+child.path" :key="child.name">
-            <el-menu-item  style=" text-align:left;" :index="item.path+'/'+child.path">
-              <svg-icon  v-if="child.meta&&child.meta.icon" :icon-class="child.meta.icon"></svg-icon>
-              <span v-if="child.meta&&child.meta.title">{{child.meta.title}}</span>
-            </el-menu-item>
-          </router-link>
-        </template>
-      </el-submenu>
+          <!--<router-link v-else :to="item.path+'/'+child.path" :key="child.name">-->
+            <!--<el-menu-item  style=" text-align:left;" :index="item.path+'/'+child.path">-->
+              <!--<svg-icon  v-if="child.meta&&child.meta.icon" :icon-class="child.meta.icon"></svg-icon>-->
+              <!--<span v-if="child.meta&&child.meta.title">{{child.meta.title}}</span>-->
+            <!--</el-menu-item>-->
+          <!--</router-link>-->
+        <!--</template>-->
+      <!--</el-submenu>-->
 
-    </template>
+    <!--</template>-->
   </div>
 </template>
 <style>
